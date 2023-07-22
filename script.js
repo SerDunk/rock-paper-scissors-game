5dcc571814f54300df1b9958593576beea32220e
SerDunk <abhinavkondapalli7@gmail.com>
2023-07-22 19:09:12 +0530

    Player choice input from user

--- script.js
+++ script.js
@@ -3,4 +3,11 @@ function getComputerChoice() {
     const randomIndex = Math.floor(Math.random() * compChoice.length);
     return compChoice[randomIndex];
 }
+function getPlayerChoice() {
+    let playerChoice = prompt("Enter your choice (rock, paper, or scissors)").toLowerCase();
+    
+    return playerChoice;
+}
+
+
 
