164dca7af67197d5c3b7d9327c195ed44152b15d
SerDunk <abhinavkondapalli7@gmail.com>
2023-08-21 15:12:17 +0530

    Finished rock-paper-scissor game with display screen showing user score,computer score and overall result of the game.Game is first to 3

--- script.js
+++ script.js
@@ -1,75 +1,73 @@
+const rock = document.querySelector('.rock1');
+const paper = document.querySelector('.paper1');
+const scissor = document.querySelector('.scissor1');
+const screen=document.querySelector('.output');
 
-    const rock = document.querySelector('.rock1');
-    const paper = document.querySelector('.paper1');
-    const scissor = document.querySelector('.scissor1');
+function getPlayerChoice(e) {
+    let playerChoice = e.target.className;
+    
+    return playerChoice;
+}
 
-    function getPlayerChoice(e){
-        let playerChoice=e.target.className
-        console.log(playerChoice)
+rock.addEventListener('click', playGame);
+paper.addEventListener('click', playGame);
+scissor.addEventListener('click', playGame);
+
+function getComputerChoice() {
+    const compChoice = ["rock1", "paper1", "scissor1"];
+    const randomIndex = Math.floor(Math.random() * compChoice.length);
+    
+    return compChoice[randomIndex];
+}
+
+function playRound(playerChoice, computerChoice) {
+    if (playerChoice === computerChoice) {
+        
+        return 0;
+    } else if (
+        (playerChoice === 'rock1' && computerChoice === 'paper1') ||
+        (playerChoice === 'scissor1' && computerChoice === 'rock1') ||
+        (playerChoice === 'paper1' && computerChoice === 'scissor1')
+    ) {
+        return -1; 
+    } else {
+        return 1; 
     }
+}
+const userScore=document.querySelector('.userscore')
+const compScore=document.querySelector('.compscore')
+let playerScore=0;
+let computerScore=0;
 
-    rock.addEventListener('click',getPlayerChoice);
-    paper.addEventListener('click',getPlayerChoice);
-    scissor.addEventListener('click',getPlayerChoice);
-
-
-    function getComputerChoice() {
-        const compChoice = ["rock", "paper", "scissors"];
-        const randomIndex = Math.floor(Math.random() * compChoice.length);
-        return compChoice[randomIndex];
+function playGame(e) {
+    if (playerScore>=3 || computerScore>=3){
+        screen.textContent="OVER"
+        return;
     }
+        
+    const playerChoice = getPlayerChoice(e);
+    console.log(playerChoice)
+    const compChoice = getComputerChoice();
+    console.log(compChoice)
+    const result = playRound(playerChoice, compChoice);
 
-    function playRound(playerChoice,computerChoice){
-        if (playerChoice==computerChoice){
-            return 0;
-        }
-        else if(playerChoice=='rock1' && computerChoice=='paper1' || playerChoice=='scissor1' && computerChoice=='paper1'
-        || playerChoice=='paper1' && computerChoice=='rock1'){
-            return 1;
-        }
-        else{
-            return -1;
-        } 
+   
+
+    if (result === 0) {
+        screen.textContent="DRAW";
+    } else if (result === 1) {
+        screen.textContent="YOU WIN";
+       
+        playerScore++;
+        userScore.textContent=playerScore;
+    } else {
+        screen.textContent="YOU LOSE";
+        
+        computerScore++;
+        compScore.textContent=computerScore;
     }
-
-    function game(){
-        for(let i=0;i<5;i++){
-            
-        }
-    }
-
-
-  
-  
 
     
-
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}
 
 
